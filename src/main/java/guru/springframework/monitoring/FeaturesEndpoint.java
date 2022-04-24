6987cce87d710a9188f33198528eb7862ee8936e
Tron Walseth <tronwa@gmail.com>
2022-04-24 08:52:36 +0200

    Oppdatert deps, lagt til custom endpoint og enablet alle actuator endpoints

---
+++ src/main/java/guru/springframework/monitoring/FeaturesEndpoint.java
@@ -0,0 +1,25 @@
+package guru.springframework.monitoring;
+
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.annotation.Selector;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Endpoint(id = "features")
+public class FeaturesEndpoint {
+
+    private List<String> features = List.of( "flying", "Swimming", "Dancing");
+
+    @ReadOperation
+    public List<String> features() {
+        return features;
+    }
+
+    @ReadOperation
+    public String feature(@Selector Integer id) {
+        return (id >= features.size() ? "" : features.get(id));
+    }
+}
